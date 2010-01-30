b29d9ed378c94061f8955b0d87aaa62d190c9709
mindless <mindless@@java.net>
2010-01-30 21:43:13 +0000

    [hudson-plugin-changes] add {color} tag on older tags to indicate age.  Version 1.1

--- src/main/resources/report.pl
+++ src/main/resources/report.pl
@@ -24,6 +24,7 @@ my @revsUrl = ('http://fisheye.hudson-ci.org/search/hudson/trunk/hudson/plugins/
 my $issueUrl = 'http://issues.hudson-ci.org/browse';
 my $prefix = $ARGV[0];
 my $svn = 'svn --non-interactive';
+my $today = &today_val;
 my ($ver, $tagrev, $cnt, $d1, $d2, $known, $since, $p, $x, %x);
 open(LS,"$svn ls $tags |") or die;
 while (<LS>) {
@@ -41,9 +42,12 @@ foreach $x (sort keys %x) {
   $_ = "$revsUrl[0]$p$revsUrl[1]$p$revsUrl[2]$d1$revsUrl[3]";
   $p = "[$p|$pluginUrl/$p]";
   print "| $p | | $ver | | | CURRENT\n" if $cnt == 0;
-  $since = "|$_] | since $ver | [r$tagrev|http://hudson-ci.org/commit/$tagrev] |";
-  print "| $p | [$cnt rev", ($cnt > 1 ? "s$since $d1 to $d2" : "$since $d1"), " | $known\n"
-    if ($known or $cnt > 0);
+  if ($known or $cnt > 0) {
+    $since = "|$_] | since $ver | [r$tagrev|http://hudson-ci.org/commit/$tagrev] |";
+    $d1 = &colorize($d1, $today);
+    $d2 = &colorize($d2, $today) if $cnt > 1;
+    print "| $p | [$cnt rev", ($cnt > 1 ? "s$since $d1 to $d2" : "$since $d1"), " | $known\n";
+  }
 }
 
 # List unreleased plugins
@@ -52,7 +56,8 @@ while (<LS>) {
   chomp; ($p = $_) =~ s!/$!!;
   next if exists $skipEntry{$p} or ($prefix and $p !~ /^$prefix/);
   ($cnt, $d1, $d2) = &revcount($p,0);
-  $_ = "|$revsUrl[0]$p$revsUrl[1]$p$revsUrl[2]$d1$revsUrl[3]] | $d1";
+  $_ = "|$revsUrl[0]$p$revsUrl[1]$p$revsUrl[2]$d1$revsUrl[3]] | " . &colorize($d1, $today);
+  $d2 = &colorize($d2, $today) if $cnt > 1;
   print "| [$p|$pluginUrl/$p] | [$cnt rev", ($cnt > 1 ? "s$_ to $d2" : $_), " | unreleased\n";
 }
 close LS;
@@ -68,7 +73,7 @@ sub byver {
   @a=split(/[._]/, $a);
   @b=split(/[._]/, $b);
   for ($i=0; $i<@a; $i++) {
-    return $x if ($x = $b[$i] <=> $a[$i]);
+    return $x if ($x = $b[$i] <=> $a[$i] or $x = $b[$i] cmp $a[$i]);
   }
   return @b > @a ? 1 : 0;
 }
@@ -96,5 +101,23 @@ sub revcount {
   $d = delete $knownRevs{"$plugin-".($fromrev-1)."-$cnt"};
   $d = 'Fixed: ' . join(' ', @fixed) unless ($d or !@fixed);
   return ($cnt, $d1, $d2, $d);
+}
+
+sub today_val {
+  @_ = localtime;
+  return $_[5] * 12 + $_[4] + $_[3]/31;
+}
+
+sub age_months {
+  my @x = split(/-/, $_[0]);
+  return $_[1] - (($x[0] - 1900) * 12 + ($x[1] - 1) + $x[2]/31);
+}
+
+sub colorize {
+  my $a = int(&age_months(@_)/2);
+  return $_[0] if $a <= 0;
+  $a = 5 if $a > 5;
+  @_ = ($_[0], '3', '6', '9', 'c', 'f');
+  return '{color:#' . $_[$a] . (9 - $a) . '6}' . $_[0] . '{color}';
 }
 
