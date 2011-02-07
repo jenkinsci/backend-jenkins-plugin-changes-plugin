b7293ef118510527052d287c954a488362831230
mindless <mindless@71c3de6d-444a-0410-be80-ed276b4c234a>
2011-02-07 21:56:56 +0000

    [plugin-changes-report] rename dir
    git-svn-id: https://svn.jenkins-ci.org/trunk/hudson/backend/plugin-changes-report@38541 71c3de6d-444a-0410-be80-ed276b4c234a

---
+++ upload.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+# Script to update wiki page with latest generated report
+import os
+import sys
+from xmlrpclib import Server
+
+content = sys.stdin.read()
+s = Server("http://wiki.jenkins-ci.org/rpc/xmlrpc")
+token = s.confluence1.login("jenkins",
+  open(os.environ['SECRET_DIR']+'/pwfile', 'r').readline().rstrip())
+page = s.confluence1.getPage(token, "jenkins", "Unreleased Plugin Changes")
+page["content"] = content
+s.confluence1.storePage(token, page)
+
